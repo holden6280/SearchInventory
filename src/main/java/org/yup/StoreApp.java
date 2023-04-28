1defd3d480b4ffaf546a4ca1c458c612d1b3faf0
holden6280 <emiicleghorn@gmail.com>
2023-04-28 16:55:22 -0400

    Only commit

--- src/main/java/org/yup/StoreApp.java
+++ src/main/java/org/yup/StoreApp.java
@@ -1,5 +1,9 @@
 package org.yup;
 
+import org.w3c.dom.ls.LSOutput;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -7,7 +11,7 @@ import java.util.Scanner;
 
 public class StoreApp {
     public static void main(String[] args) {
-        ArrayList<Product> inventory = getInventory();
+        ArrayList<Product> inventory = loadInventory();
         Scanner scanner = new Scanner(System.in);
 
         Collections.sort(inventory, Comparator.comparing(Product::getName));
@@ -18,17 +22,43 @@ public class StoreApp {
             System.out.printf("id: %d %s - Price: $%.2f\n", p.getId(), p.getName(), p.getPrice());
         }
     }
-    public static ArrayList<Product> getInventory() {
-        ArrayList<Product> inventory =
-                new ArrayList<Product>();
 
-        inventory.add(new Product(0,"rice cakes",2));
-        inventory.add(new Product(1,"bagged baby spinach",4));
-        inventory.add(new Product(2,"cocoa almond spread",3));
-        inventory.add(new Product(3,"dozen eggs",9));
-        inventory.add(new Product(4,"lb. tomatoes",2));
-        inventory.add(new Product(5,"gallon orange juice",3));
+    public static ArrayList<Product> loadInventory() {
 
-        return inventory;
+        try {
+            FileReader finder = new FileReader("inventory.csv");
+            BufferedReader reader = new BufferedReader(finder);
+
+            ArrayList<String> inventory = new ArrayList<>();
+
+            System.out.println(inventory);
+            System.out.println(inventory.size());
+            String theLine;
+            while ((theLine = reader.readLine()) != null) {
+
+                String[] item = theLine.split("\\|");
+
+                inventory.add(item[1]);
+
+            }
+        } catch (IOException e) {
+            System.out.println("No <3");
+        }
+/*
+        ArrayList<Product> loadInventory; {
+            ArrayList<Product> inventory = new ArrayList<Product>();
+
+
+            inventory.add(new Product(0, "rice cakes", 2));
+            inventory.add(new Product(1, "bagged baby spinach", 4));
+            inventory.add(new Product(2, "cocoa almond spread", 3));
+            inventory.add(new Product(3, "dozen eggs", 9));
+            inventory.add(new Product(4, "lb. tomatoes", 2));
+            inventory.add(new Product(5, "gallon orange juice", 3));
+
+            return inventory;
+
+
+        }*/
     }
 }
